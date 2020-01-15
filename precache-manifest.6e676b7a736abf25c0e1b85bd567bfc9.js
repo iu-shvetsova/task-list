5411ff401f14f333639f1f3175ebd0bc13bcdcb9
Julia Shvetsova <iu-shvetsova69@yandex.ru>
2020-01-15 10:17:55 +0300

    Updates

---
+++ precache-manifest.6e676b7a736abf25c0e1b85bd567bfc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b68028acd79ef064ea74fc1294fe01d",
+    "url": "/task-list/index.html"
+  },
+  {
+    "revision": "48364b8c29668cd311e2",
+    "url": "/task-list/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "301b900d726ed9e4810f",
+    "url": "/task-list/static/css/main.da4e1c9d.chunk.css"
+  },
+  {
+    "revision": "48364b8c29668cd311e2",
+    "url": "/task-list/static/js/2.096f614b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/task-list/static/js/2.096f614b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "301b900d726ed9e4810f",
+    "url": "/task-list/static/js/main.d785aa7e.chunk.js"
+  },
+  {
+    "revision": "1f560d6372710517df18",
+    "url": "/task-list/static/js/runtime-main.c5087af8.js"
+  }
+]);
